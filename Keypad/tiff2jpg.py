2b06238131dfb49d609dd7d102f262e852f2fd86
Md Shafiqul Islam <shafiq06eee@gmail.com>
2022-09-20 21:46:21 +0100

    Add files via upload

---
+++ Keypad/tiff2jpg.py
@@ -0,0 +1,11 @@
+import cv2, os, shutil
+base_path = "C:/Users/shafi/Desktop/GSF/Demo/"       
+new_path = "C:/Users/shafi/Desktop/GSF/Demo/"
+d=1
+for infile in os.listdir(base_path):
+    print ("file : " + infile)
+    read = cv2.imread(base_path + infile)
+    #outfile = infile.split('.')[0] + '.jpg'
+    outfile = '%d.jpg'%d
+    cv2.imwrite(new_path+outfile,read,[int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    d+=1
